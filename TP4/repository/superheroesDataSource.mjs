c47a622a0607397c202f1e4ba15b1a44d7fa732d
Sergio <sergitula.50@gmail.com>
2026-03-07 21:37:41 -0300

    Agrego desarrollo TP4

---
+++ TP4/repository/superheroesDataSource.mjs
@@ -0,0 +1,6 @@
+export default class SuperheroesDataSource {
+    // Método abstracto para obtener todos los superhéroes
+    obtenerTodos() {
+        throw new Error('Este método debe ser implementado por la subclase');
+    }
+}
---
+++ TP4/server.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import {
+    obtenerSuperheroePorIdController, 
+    buscarSuperheroesPorAtributoController, 
+    obtenerSuperheroesMayoresDe30Controller
+} from './controllers/superheroesController.mjs';
+
+const app = express();
+const PORT = 3005;
+
+
+
+app.get('/superheroes/atributo/:atributo/:valor', buscarSuperheroesPorAtributoController);
+//http://localhost:3005/superheroes/id/1 (Spiderman)
+//http://localhost:3005/superheroes/id/99 (No encontrado)
+app.get('/superheroes/id/:id', obtenerSuperheroePorIdController);
+//Por planeta: http://localhost:3005/superheroes/atributo/planetaOrigen/Tierra (Spiderman e Ironman)
+app.get('/superheroes/edad/mayorA30', obtenerSuperheroesMayoresDe30Controller);
+// http://localhost:3005/superheroes/edad/mayorA30  (Ironman (45años))
+
+
+// Levantar el servidor en el puerto 3005
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
